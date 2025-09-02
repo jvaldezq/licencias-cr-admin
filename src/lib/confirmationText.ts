2242234b37f6554a89cdf4fb8f8f1b52abec4f2d
Jordan Valdez <jvaldezq1101@gmail.com>
2025-09-02 14:15:00 -0600

    Update confirmation test

--- src/lib/confirmationText.ts
+++ src/lib/confirmationText.ts
@@ -3,13 +3,19 @@ export const cvClass = (
 ) => `Te recordamos tu cita para el día mañana. Debes presentarte a las ${time} en nuestra oficina que se encuentra 75 mts Oeste de entrada principal de COSEVI Ciudad Vial. 
 Esta sería nuestra ubicación en Waze: https://waze.com/ul/hd1u0qu1zm
 
-*Por favor ser puntual, en caso de algún retraso, podrías perder ese tiempo de preparación.*`;
+*Por favor ser puntual, en caso de algún retraso, podrías perder ese tiempo de preparación.*
+
+**Le informamos que en nuestra oficina el método de pago autorizado es exclusivamente en efectivo.
+No recibimos sinpe móvil, tarjeta ni transferencia bancaria.**`;
 export const srClass = (
   time: string,
 ) => `Te recordamos tu cita para mañana. Debes presentarte a las ${time} en nuestra oficina que se encuentra ubicada 50 mts Oeste del Plantel de COSEVI San Ramón. 
 Esta sería nuestra ubicación en Waze: https://waze.com/ul/hd1gcmvhkm
 
-*Por favor ser puntual, en caso de algún retraso, podrías perder ese tiempo de preparación.*`;
+*Por favor ser puntual, en caso de algún retraso, podrías perder ese tiempo de preparación.*
+
+**Le informamos que en nuestra oficina el método de pago autorizado es exclusivamente en efectivo.
+No recibimos sinpe móvil, tarjeta ni transferencia bancaria.**`;
 export const srTest = (
   time: string,
 ) => `Te recordamos tu cita para mañana. Debes presentarte a las ${time} en nuestra oficina que se encuentra ubicada 50 mts Oeste del Plantel de COSEVI San Ramón. 
@@ -17,7 +23,10 @@ Esta sería nuestra ubicación en Waze: https://waze.com/ul/hd1gcmvhkm
 
 *Por favor ser puntual para poder realizar la preparación completa.*
 
-Te envío el reglamento de evaluación para que tengas claro los puntos a evaluar en la prueba: https://firebasestorage.googleapis.com/v0/b/carrocr-6bcb0.appspot.com/o/Reglamento.pdf?alt=media&token=f9223640-3f81-400d-89f2-5a9bb5663ff8`;
+Te envío el reglamento de evaluación para que tengas claro los puntos a evaluar en la prueba: https://firebasestorage.googleapis.com/v0/b/carrocr-6bcb0.appspot.com/o/Reglamento.pdf?alt=media&token=f9223640-3f81-400d-89f2-5a9bb5663ff8
+
+**Le informamos que en nuestra oficina el método de pago autorizado es exclusivamente en efectivo.
+No recibimos sinpe móvil, tarjeta ni transferencia bancaria.**`;
 export const cvTest = (
   time: string,
 ) => `Te recordamos tu cita para el día mañana. Debes presentarte a las ${time} en nuestra oficina que se encuentra 75 mts Oeste de entrada principal de COSEVI Ciudad Vial. 
@@ -25,4 +34,7 @@ Esta sería nuestra ubicación en Waze: https://waze.com/ul/hd1u0qu1zm
 
 *Por favor ser puntual para poder realizar la preparación completa.*
 
-Te envío el reglamento de evaluación para que tengas claro los puntos a evaluar en la prueba: https://firebasestorage.googleapis.com/v0/b/carrocr-6bcb0.appspot.com/o/Reglamento.pdf?alt=media&token=f9223640-3f81-400d-89f2-5a9bb5663ff8`;
+Te envío el reglamento de evaluación para que tengas claro los puntos a evaluar en la prueba: https://firebasestorage.googleapis.com/v0/b/carrocr-6bcb0.appspot.com/o/Reglamento.pdf?alt=media&token=f9223640-3f81-400d-89f2-5a9bb5663ff8
+
+**Le informamos que en nuestra oficina el método de pago autorizado es exclusivamente en efectivo.
+No recibimos sinpe móvil, tarjeta ni transferencia bancaria.**`;
